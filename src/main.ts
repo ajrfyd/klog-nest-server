0e5d39762b2127ebc7acc4635f7f311a97566738
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-11-14 22:45:11 +0900

    Fix: cors

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
 
   app.enableCors({
     methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE'],
-    origin: ['http://localhost:3000', 'https://blog.hkound.shop'],
+    origin: ['http://localhost:3000', 'https://blog.hkound.pe.kr'],
     credentials: true,
   });
 
